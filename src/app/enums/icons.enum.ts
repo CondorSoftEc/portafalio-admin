163f0574a02e46773a288f374dec1972381fc059
André Herrera <andreherrera97@hotmail.com>
2021-04-16 17:18:14 -0500

    Adding new icons

--- src/app/enums/icons.enum.ts
+++ src/app/enums/icons.enum.ts
@@ -3,5 +3,8 @@ export enum Icons {
   github = 'bi bi-github',
   twitter = 'bi bi-twitter',
   youtube = 'bi bi-youtube',
-
+  web = 'bi bi-globe',
+  linkedin = 'bi bi-linkedin',
+  instagram = 'bi bi-instagram',
+  telegram = 'bi bi-telegram'
 }
